1fe77482fa337e319cc686b472f522bd30c7f31a
Wojciech Latek <72910178+animestanduser@users.noreply.github.com>
2022-12-06 22:48:39 +0100

    I made function to progress the progress-bar

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ $(document).ready(function () {
 
 });
 
+function addProgress() {
+    // Get the checkbox
+    var checkBox = document.getElementById("flexCheckDefault");
+    // Get the output element
+    var progressBar1 = document.getElementById("progressBar1");
+
+    var percent = 10;
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        percent = percent > 90 ? 10 : percent + 10;
+        document.querySelector(".progress-bar").style.width = percent + "%";
+    } else {
+        percent = percent > 90 ? 10 : percent - 10;
+        document.querySelector(".progress-bar").style.width = percent + "%";
+    }
+}
