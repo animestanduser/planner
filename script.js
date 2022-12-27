3a7108c7d0dce3affb9d445119cd2f3f3b46eab3
Wojciech Latek <72910178+animestanduser@users.noreply.github.com>
2022-12-27 21:40:35 +0100

    Adding a new item for a task-list is working.

--- script.js
+++ script.js
@@ -49,3 +49,32 @@ function createNewTask(){
     newTask.taskName = newTaskForm.taskNameForm.value;
     document.getElementById("item-workspace").innerHTML += '<div class="item-task border-bottom"><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" onclick="addProgress(this)"><label class="form-check-label" for="flexCheckDefault">' + newTask.taskName + '</label></div></div>';     
 }
+
+
+// function for adding a new task list object
+/*
+function TaskList(name){
+    this.taskListName = ""
+
+}
+
+var newTaskList = new TaskList();
+
+function createNewTaskList(){
+    newTaskList.taskListName = "Task List";
+    document.getElementById("container").innerHTML += '<div class="col-md-2 item border"><div class="item-header border-bottom"><form name="newTaskForm" id="formForTask" target="stupidFrame"><input type="text" name="taskNameForm" placeholder="Write new task name" class="input-field"><input type="submit" id="submit" name="saveTaskForm" onclick="createNewTask()" class="input-field-button"><i class="fa-solid fa-plus"></i></input></form></div><div class="item-workspace" id="item-workspace"><iframe name="stupidFrame" id="stupidFrame" style="display: none;"></iframe><div class="item-progress-space"><div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar"aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:0%" id="progressbar">percentage</div></div></div></div>';
+}
+*/
+
+//Task lists
+//create an html object that will contain the checkboxes
+//make a function for progressing the progress-bar once checkbox will be clicked and make it smaller if its not clicked
+//change the value of increasing the length of the progressed-bar based on number of added objects
+//each one of the new objects should be independent, based on the constructors
+// prop to be done in the for loop
+
+
+
+
+
+// ??? would it be better with jquery ???
