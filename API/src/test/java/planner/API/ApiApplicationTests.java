d56ae199bae303f880be6f76833dcc1df4c92de5
Wojciech Latek <72910178+animestanduser@users.noreply.github.com>
2023-01-03 15:52:32 +0100

    Created the solids for the micro-service.

---
+++ API/src/test/java/planner/API/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package planner.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
